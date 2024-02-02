27c9ea7c215853a5e8c9da9dd1cc76c1398b9b9b
Becky Sweger <bsweger@gmail.com>
2024-02-02 14:24:16 -0500

    udpate install.sh to address issues when testing the setup

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 conda create -n llmtime python=3.9
 conda activate llmtime
 pip install numpy
-pip install -U jax[cpu] # we don't need GPU for jax
-pip install torch --index-url https://download.pytorch.org/whl/cu118
+pip install -U "jax[cpu]" # we don't need GPU for jax
+# pip was unable to resolve pytorch using the specified index-url, so
+# let try without and let pip's resolver sort it out
+# pip install torch --index-url https://download.pytorch.org/whl/cu118
+pip install torch
 pip install openai==0.28.1
 pip install tiktoken
 pip install tqdm
